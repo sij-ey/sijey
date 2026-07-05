a9f83b1481773477417a49a50c443254ab6901e7
sij-ey <charlseochieng01@gmail.com>
2026-07-05 21:06:58 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const About = lazy(() => import("./components/About"));
 const Experience = lazy(() => import("./components/Experience"));
 const Projects = lazy(() => import("./components/Projects"));
 const Skills = lazy(() => import("./components/Skills"));
-const Education = lazy(() => import("./components/Education"));
 const Contact = lazy(() => import("./components/Contact"));
 const Footer = lazy(() => import("./components/Footer"));
 const Services = lazy(() => import("./components/Services"));
